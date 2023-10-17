974fa76e5e7c636ccb6a21c2b816cce6b82dfd59
Damien Henry <damien@initml.co>
2023-10-17 22:27:28 +0200

    add a script to download the audio from video

---
+++ scripts/download_audio.py
@@ -0,0 +1,30 @@
+# This script downloads videos from YouTube using pytube
+# It reads a CSV file with the URLs of the videos to download
+
+import csv
+from pytube import YouTube
+
+
+def download_audio(url, path):
+    yt = YouTube(url)
+    audio_stream = yt.streams.filter(only_audio=True).first()
+    audio_stream.download(output_path=path)
+
+
+def read_csv_and_download(filename, path="~/Downloads"):
+    with open(filename, "r", newline="", encoding="utf-8") as csvfile:
+        csvreader = csv.reader(csvfile)
+        next(csvreader)  # Skip the header
+        for row in csvreader:
+            try:
+                print(f"Downloading {row[0]}")
+                download_audio(
+                    row[0], path
+                )  # Assuming URL is in the first column of the CSV
+            except Exception as e:
+                print(f"Error downloading {row[0]}: {e}")
+
+
+if __name__ == "__main__":
+    csv_filename = "../sources/youtube.csv"
+    read_csv_and_download(csv_filename, path="../data/audio/")
